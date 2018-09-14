2e63b3138534cdd738348291251b139d6a4d24d2
Alejandro Osorio Trujillo <30884745+alejoso76@users.noreply.github.com>
2018-09-14 07:46:50 -0500

    Se agrega el analizador lexico

---
+++ Proyecto/analizadorLexico.py
@@ -0,0 +1,30 @@
+import ply.lex as lex
+import sys
+
+tokens = {
+    'IF',
+    'ELSE',
+    'ELSEIF',
+    'ENDIF',
+    'FOR',
+    'WHILE',
+    'DO',
+    'CLASS',
+    'PUBLIC',
+    'PRIVATE',
+    'PROTECTED',
+    'RETURN',
+    'AND',
+    'OR',
+    'NOT',
+    'PRINT',
+    'VAR',
+    'BREAK',
+    'CASE',
+    'INCLUDE',
+    'GLOBAL',
+    'ENDWHILE',
+    'XOR',
+    
+
+}
